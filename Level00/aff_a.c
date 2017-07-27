099fb4e29603393d6c3412526fe1d6e400214b63
Gary <garyowtc@gmail.com>
2017-07-27 03:38:56 -0400

    Practice Answers

---
+++ Level00/aff_a.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	if (argc != 2)
+	{
+		ft_putchar('a');
+		ft_putchar('\n');
+		return (0);
+	}
+	while (argv[1][i])
+	{
+		if(argv[1][i] == 'a')
+			ft_putchar('a');
+		i++;
+	}
+	ft_putchar('\n');
+	return (0);
+}
